8741876153ceef22b836d79d4c6ec880201f3bdf
drojf <1249449+drojf@users.noreply.github.com>
2024-03-31 18:57:23 +1100

    Extract and verify mod character

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ outputLineAll = re.compile(r"OutputLineAll\([^;]*;")
 
 outputLineRegex = re.compile(r"OutputLine\(\s*[^,]*,\s*\"(.*)\"\s*,")
 
-spritePathRegex = re.compile(r'\"sprites/([^/]+)')
+ogSpritePathCharacterNameRegex = re.compile(r'"sprites/([^/]+)')
+
+modSpritePathCharacterNameRegex = re.compile(r'"((?:sprite)|(?:portrait))/([a-zA-Z]*)')
 
 # unmodded_input_file = 'C:/Program Files (x86)/Steam/steamapps/common/Higurashi When They Cry Hou+ Installer Test/HigurashiEp10_Data/StreamingAssets/Scripts/mehagashi.txt'
 mod_script_dir = 'C:/drojf/large_projects/umineko/HIGURASHI_REPOS/10 hou-plus/Update/'
@@ -32,7 +34,30 @@ modded_input_file = os.path.join(mod_script_dir, mod_script_file)
 
 #     return None
 
+RENA = 'rena'
+KEIICHI = 'keiichi'
+SHION = 'shion'
+RIKA = 'rika'
+HANYU = 'hanyu'
+SATOKO = 'satoko'
+MION = 'mion'
+IRIE = 'irie'
+MOB_CHARACTER = 'mob'
+KAMEDA = 'kameda'
 
+
+mod_to_name = {
+    're': RENA,
+    'si': SHION,
+    'kei': KEIICHI,
+    'ri': RIKA,
+    'ha': HANYU,
+    'sa': SATOKO,
+    'me': MION,
+    'iri': IRIE,
+    'mo': MOB_CHARACTER,
+    'kameda': KAMEDA
+}
 
 def get_vanilla_only(log_lines):
 
@@ -72,13 +97,24 @@ def get_original_line(mod_script_dir, mod_script_file, line_no) -> List[str]:
 
 def parse_line(mod_script_dir, mod_script_file, all_lines, line_index, line):
     if 'ModDrawCharacter' in line:
-        print(f"Line No: {line_index + 1}")
-        print(line.strip())
+        match = modSpritePathCharacterNameRegex.search(line)
+        if match:
+            # 'sprite' or 'portrait'
+            sprite_type = match.group(1)
+            mod_character = match.group(2)
+        else:
+            raise Exception(f"No character found in line {line}")
+
+        print(f"Line No: {line_index + 1} Type: {sprite_type} Char: {mod_character} Line: {line.strip()}")
+
+        if mod_character not in mod_to_name:
+            raise Exception(f"No mod character {mod_character} in database")
+
         original_lines = get_original_line(mod_script_dir, mod_script_file, line_index + 1)
 
         for line in original_lines:
             # determine what character is being displayed
-            match = spritePathRegex.search(line)
+            match = ogSpritePathCharacterNameRegex.search(line)
             if match:
                 character = match.group(1)
                 print(f'{character}: {line.strip()}')
